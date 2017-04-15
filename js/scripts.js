4d2d642d71e0eb0804e14b7dd88fb02c41af3d0a
HLA-systeem <aaakanten@outlook.com>
2017-04-16 01:16:36 +0200

    De versie zoals die tijdens de tussenmarkt was

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Sonny on 4/5/2017.
+ */
+function countdown(element, minutes, seconds) {
+    // Fetch the display element
+    var el = document.getElementById(element);
+
+    // Set the timer
+    var interval = setInterval(function() {
+        if(seconds == 0) {
+            if(minutes == 0) {
+                (el.innerHTML = "KOMT ERAAN!");
+
+                clearInterval(interval);
+                return;
+            } else {
+                minutes--;
+                seconds = 60;
+            }
+        }
+
+        if(minutes > 0) {
+            var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+        } else {
+            var minute_text = '';
+        }
+
+        var second_text = seconds > 1 ? '' : '';
+        el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + '';
+        seconds--;
+    }, 1000);
+}
+
+//Start as many timers as you want
+
+var start1 = document.getElementById("timer1");
+var start2 = document.getElementById('timer2');
+var start3 = document.getElementById('timer3');
+var start4 = document.getElementById('timer4');
+var start5 = document.getElementById('timer5');
+
