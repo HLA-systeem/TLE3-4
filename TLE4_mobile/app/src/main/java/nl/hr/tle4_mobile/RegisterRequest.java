ce64b8534fcfb48a7597d4de55786b737e48c0e0
HLA-systeem <aaakanten@outlook.com>
2017-05-30 10:51:38 +0200

    reverted

---
+++ TLE4_mobile/app/src/main/java/nl/hr/tle4_mobile/RegisterRequest.java
@@ -0,0 +1,87 @@
+package nl.hr.tle4_mobile;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+public class RegisterRequest extends AsyncTask<String,Void,String> {
+    private WeakReference<RegisterActivity> activityRef;
+    String registerURL = "https://brusque-millimeters.000webhostapp.com/register.php";
+
+    public RegisterRequest(RegisterActivity context){
+        this.activityRef = new WeakReference<RegisterActivity>(context);
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String username = params[0];
+        String password = params[1];
+
+        try {
+            URL url = new URL(this.registerURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            OutputStream outputStream = con.getOutputStream();
+            BufferedWriter bufw = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+            String postData = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") + "&" +
+                    URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+            bufw.write(postData);
+            bufw.flush();
+            bufw.close();
+            outputStream.close();
+
+            InputStream inputStream = con.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+            String result="";
+            String line="";
+            while((line = bufferedReader.readLine())!= null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+
+            con.disconnect();
+
+            return result;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute(){
+
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        System.out.println(result);
+        Intent in = new Intent(this.activityRef.get(), TimerActivity.class);
+        this.activityRef.get().startActivity(in);
+    }
+
+    @Override
+    protected void onProgressUpdate(Void...values){
+        super.onProgressUpdate(values);
+    }
+}
---
+++ TLE4_mobile/app/src/main/java/nl/hr/tle4_mobile/SchipholApi.java
@@ -0,0 +1,116 @@
+package nl.hr.tle4_mobile;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by sonny on 5/29/2017.
+ */
+
+public class SchipholApi extends AsyncTask<String, Void, String> {
+    private WeakReference<FlightStatusActivity> flightActivity;
+
+    public SchipholApi(FlightStatusActivity activity){
+        flightActivity = new WeakReference<FlightStatusActivity>(activity);
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        String result ="";
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            url = new URL(urls[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoInput(true);
+            urlConnection.setRequestProperty("ResourceVersion", "v3");
+            urlConnection.setRequestProperty("Content-Type","application/json");
+            urlConnection.connect();
+
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+
+            int data = reader.read();
+
+            while(data != -1){
+                char current  = (char)data;
+                result += current;
+                data = reader.read();
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        super.onPostExecute(result);
+
+        String results;
+
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            String flight = jsonObject.getString("flights");
+
+            JSONArray arr = new JSONArray(flight);
+
+            for(int i = 0; i < arr.length(); i++){
+                JSONObject jsonObject1 = arr.getJSONObject(i);
+
+                String flightname = jsonObject1.getString("flightName");
+                String landingTime = jsonObject1.getString("estimatedLandingTime");
+                String estimatedTimeOnBelt = jsonObject1.getString("expectedTimeOnBelt");
+                String test = jsonObject1.getString("checkinAllocations");
+
+                // get baggage belt from an array and convert it into string
+                JSONObject baggageObject = jsonObject1.getJSONObject("baggageClaim");
+                String baggageBelt = baggageObject.getString("belts");
+                JSONArray baggageArray = new JSONArray(baggageBelt);
+                String belt = baggageArray.getString(0);
+
+                Log.i("flightinfo", belt);
+                Log.i("flightinfo", estimatedTimeOnBelt);
+                Log.i("flightinfo;", flightname);
+
+                if(test == "null") {
+                    Log.i("flightinfo", "test came back null");
+                }else {
+                    Log.i("flightinfo", test);
+                }
+
+                results = "Flight name: " + flightname + "\r\n"
+                        + "Baggage belt: " + belt + "\r\n"
+                        + "Estimated time on belt: " + estimatedTimeOnBelt;
+
+                flightActivity.get().setInfo(results);
+
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
